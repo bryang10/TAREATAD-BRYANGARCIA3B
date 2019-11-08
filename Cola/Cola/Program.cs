7972377ba18275642cd0b5df8381b17fdcfb476c
bryang10 <56742984+bryang10@users.noreply.github.com>
2019-11-07 23:34:47 -0500

    Add files via upload

---
+++ Cola/Cola/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Cola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string men;
+
+            Queue cola = new Queue();
+
+            cola.Enqueue("BRYAN");
+            cola.Enqueue("ALEJANDRO");
+            cola.Enqueue("GARCIA");
+            cola.Enqueue("MENDOZA");
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                men = (string)cola.Dequeue();
+                Console.Write(men+ " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
