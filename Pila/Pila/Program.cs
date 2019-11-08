bcb1044615331383d50f804291162f7edf6d61fe
bryang10 <56742984+bryang10@users.noreply.github.com>
2019-11-07 23:35:39 -0500

    Add files via upload

---
+++ Pila/Pila/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Pila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string men;
+            // stack  es desapilar - clases preconstruidas
+            Stack pila = new Stack();
+
+            pila.Push("BRYAN");
+            pila.Push("ALEJANDRO");
+            pila.Push("GARCIA");
+            pila.Push("MENDOZA");
+            
+
+            for (int i = 0; i < 4; i++)
+            {
+                men= (string)pila.Pop();
+                Console.WriteLine(men + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
